64381cfeab0bb16eb9082b1f377131da016e3d49
Wilker V. Santos <wilker.vinicios@hotmail.com>
2025-02-05 02:16:31 -0300

    008 - Criado controller para tarefas.

---
+++ fonte/src/main/java/com/br/stefanini/controller/TarefaController.java
@@ -0,0 +1,44 @@
+package com.br.stefanini.controller;
+
+import com.br.stefanini.dto.TarefaDTO;
+import com.br.stefanini.service.TarefaService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefa")
+public class TarefaController {
+
+    private final TarefaService tarefaService;
+
+    public TarefaController(TarefaService tarefaService) {
+        this.tarefaService = tarefaService;
+    }
+
+    @GetMapping("/listarTarefas")
+    public List<TarefaDTO> listarTarefas() {
+        return tarefaService.listarTarefas();
+    }
+
+    @GetMapping("/listarTarefas/{id}")
+    public TarefaDTO buscarTarefaPorId(@PathVariable Long id) {
+        return tarefaService.buscarPorId(id);
+    }
+
+    @PostMapping("/salvar")
+    public void salvarTarefa(@Validated @RequestBody TarefaDTO tarefa) {
+        tarefaService.salvarTarefa(tarefa);
+    }
+
+    @PutMapping("/editar/{id}")
+    public void editarTarefa(@PathVariable Long id, @Validated @RequestBody TarefaDTO tarefaDto) {
+        tarefaService.editarTarefa(id, tarefaDto);
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public void excluirTarefa(@PathVariable Long id) {
+        tarefaService.excluirTarefa(id);
+    }
+}
