1593bd89ec4747ecdae68aea1be4eef7aaa99ef6
Wilker V. Santos <wilker.vinicios@hotmail.com>
2025-02-04 21:53:03 -0300

    005 - Criado exceptions personalizadas para a aplicação.

---
+++ fonte/src/main/java/com/br/stefanini/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.br.stefanini.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
